e58989fc984b8350cacd82bbe7135e7081543a23
hatelove <jay.hatelove@gmail.com>
2017-12-13 16:34:32 +0800

    rename DiceTest test case names

--- tests/DiceTest.php
+++ tests/DiceTest.php
@@ -8,32 +8,32 @@ use PHPUnit\Framework\TestCase;
 class DiceTest extends TestCase
 {
     protected $dice;
-    
+
     /**
      * @test
      * @group DiceTest
      */
-    public function testGetUniqueCount()
+    public function testGetUniqueCount_sameColor()
     {
         $input = [2, 2, 2, 2];
         $this->dice = new Dice($input);
-        
+
         $act = $this->dice->getUniqueCount();
-        
+
         $this->assertEquals($act, 1);
     }
-    
+
     /**
      * @test
      * @group DiceTest
      */
-    public function testGroupDiceValueCount()
+    public function testGroupDiceValueCount_sameColor()
     {
         $input = [2, 2, 2, 2];
         $this->dice = new Dice($input);
-        
+
         $act = $this->dice->groupDice();
-        
+
         $this->assertEquals([2 => 4], $act);
     }
 }
