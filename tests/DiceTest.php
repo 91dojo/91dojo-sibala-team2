18f3fc03b7d5cc10f5ec3563ca1a7b6e7a1c4134
hatelove <jay.hatelove@gmail.com>
2017-12-14 13:54:19 +0800

    refactor testing: rename test cases and add test cases with no points, normal points 1 pair and 2 pairs

--- tests/DiceTest.php
+++ tests/DiceTest.php
@@ -8,32 +8,116 @@ use PHPUnit\Framework\TestCase;
 class DiceTest extends TestCase
 {
     protected $dice;
-    
+
     /**
      * @test
      * @group DiceTest
      */
-    public function testGetUniqueCount()
+    public function testGetUniqueCount_sameColor()
     {
         $input = [2, 2, 2, 2];
         $this->dice = new Dice($input);
-        
+
+        $this->uniqueCountShouoldBe(1);
+    }
+
+    private function uniqueCountShouoldBe($expected): void
+    {
         $act = $this->dice->getUniqueCount();
-        
-        $this->assertEquals($act, 1);
+
+        $this->assertEquals($expected, $act);
     }
-    
+
     /**
      * @test
      * @group DiceTest
      */
-    public function testGroupDiceValueCount()
+    public function testGetUniqueCount_noPoints()
+    {
+        $input = [2, 3, 4, 6];
+        $this->dice = new Dice($input);
+
+        $this->uniqueCountShouoldBe(4);
+    }
+
+    /**
+     * @test
+     * @group DiceTest
+     */
+    public function testGetUniqueCount_normalPoints_1pair()
+    {
+        $input = [2, 4, 4, 6];
+        $this->dice = new Dice($input);
+
+        $this->uniqueCountShouoldBe(3);
+    }
+
+    /**
+     * @test
+     * @group DiceTest
+     */
+    public function testGetUniqueCount_normalPoints_2pair()
+    {
+        $input = [3, 4, 4, 3];
+        $this->dice = new Dice($input);
+
+        $this->uniqueCountShouoldBe(2);
+    }
+
+    /**
+     * @test
+     * @group DiceTest
+     */
+    public function testGroupDiceValueCount_sameColor()
     {
         $input = [2, 2, 2, 2];
         $this->dice = new Dice($input);
-        
-        $act = $this->dice->groupDice();
-        
-        $this->assertEquals([2 => 4], $act);
+
+        $this->groupDiceShouldBe([2 => 4]);
     }
+
+    private function groupDiceShouldBe($expected): void
+    {
+        $act = $this->dice->groupDice();
+
+        $this->assertEquals($expected, $act);
+    }
+
+    /**
+     * @test
+     * @group DiceTest
+     */
+    public function testGroupDiceValueCount_noPoints()
+    {
+        $input = [3, 2, 5, 6];
+        $this->dice = new Dice($input);
+
+        $this->groupDiceShouldBe([3 => 1, 2 => 1, 5 => 1, 6 => 1]);
+    }
+
+    /**
+     * @test
+     * @group DiceTest
+     */
+    public function testGroupDiceValueCount_normalPoints_1pair()
+    {
+        $input = [3, 5, 5, 6];
+        $this->dice = new Dice($input);
+
+        $this->groupDiceShouldBe([3 => 1, 5 => 2, 6 => 1]);
+    }
+
+    /**
+     * @test
+     * @group DiceTest
+     */
+    public function testGroupDiceValueCount_normalPoints_2pair()
+    {
+        $input = [3, 5, 5, 3];
+        $this->dice = new Dice($input);
+
+        $this->groupDiceShouldBe([3 => 2, 5 => 2]);
+    }
+
+
 }
