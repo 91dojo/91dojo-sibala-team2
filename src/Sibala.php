6fb8f08e763cf3b16ef638b7c7a193afdb189141
hatelove <jay.hatelove@gmail.com>
2017-12-14 14:17:25 +0800

    refactor: extract method by responsibility

--- src/Sibala.php
+++ src/Sibala.php
@@ -27,25 +27,16 @@ class Sibala
             return "Same Color";
         }
 
-        if ($this->getPoints() === 3) {
-            return "BG";
-        } elseif ($this->getPoints() === 12) {
-            return "Sibala";
-        }
-
-        return $this->getPoints() . " Points";
+        return $this->getOutputWhenNormalPoints();
     }
 
     public function getState()
     {
         $maxCountOfSamePoint = collect($this->dice->groupDice())->max();
-        if ($maxCountOfSamePoint === 3) {
-            return $this::NO_POINTS;
-        }
 
         $uniqueCount = $this->dice->getUniqueCount();
 
-        if ($uniqueCount === 4) {
+        if ($uniqueCount === 4 || $maxCountOfSamePoint === 3) {
             return $this::NO_POINTS;
         }
 
@@ -56,41 +47,105 @@ class Sibala
         return $this::N_POINTS;
     }
 
+    /**
+     * @return string
+     */
+    private function getOutputWhenNormalPoints(): string
+    {
+        if ($this->getPoints() === 3) {
+            return "BG";
+        } elseif ($this->getPoints() === 12) {
+            return "Sibala";
+        }
+
+        return $this->getPoints() . " Points";
+    }
+
     public function getPoints()
     {
         if ($this->getState() === SELF::NO_POINTS) {
-            return 0;
-        } elseif ($this->getState() === SELF::SAME_POINTS) {
+            return $this->getPointsWhenNoPoints();
+        }
+
+        if ($this->getState() === SELF::SAME_POINTS) {
             //回傳陣列隨便的一個值 (SAME_POINTS)
-            return $this->dice->getNumber()[0];
-        } else {
-            $groupDice = $this->dice->groupDice();
-            ksort($groupDice);
-            if (count($groupDice) === 2) {
-                //2,2,4,4的情況
-                if (last($groupDice) === 2) {
-                    return last(array_keys($groupDice)) * 2;
-                } else {
-                    //4,4,4,2的情況
-                    return collect($groupDice)->keys()->sum();
-                }
+            return $this->getPointsWhenSameColor();
+        }
+        return $this->getPointsWhenNormalPoints();
+    }
+
+    /**
+     * @return int
+     */
+    private function getPointsWhenNoPoints(): int
+    {
+        return 0;
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getPointsWhenSameColor()
+    {
+        return $this->dice->getNumber()[0];
+    }
+
+    /**
+     * @return float|int
+     */
+    private function getPointsWhenNormalPoints()
+    {
+        $groupDice = $this->dice->groupDice();
+        ksort($groupDice);
+        if (count($groupDice) === 2) {
+            //2,2,4,4的情況
+            if (last($groupDice) === 2) {
+                return last(array_keys($groupDice)) * 2;
+            } else {
+                //4,4,4,2的情況
+                return collect($groupDice)->keys()->sum();
             }
-            if (count($groupDice) === 3) {
-                return collect($groupDice)->reject(function ($item) {
-                    return $item > 1;
-                })->keys()->sum();
-            }
+        }
+        if (count($groupDice) === 3) {
+            return collect($groupDice)->reject(function ($item) {
+                return $item > 1;
+            })->keys()->sum();
         }
     }
 
     public function getMaxNumber()
     {
         if ($this->getState() === SELF::NO_POINTS) {
-            return 0;
+            return $this->getMaxNumberWhenNoPoints();
         }
         if ($this->getState() === SELF::SAME_POINTS) {
-            return $this->dice->getNumber()[0];
+            return $this->getMaxNumberWhenSameColor();
         }
+        return $this->getMaxNumberWhenNormalPoints();
+
+    }
+
+    /**
+     * @return int
+     */
+    private function getMaxNumberWhenNoPoints(): int
+    {
+        return 0;
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getMaxNumberWhenSameColor()
+    {
+        return $this->dice->getNumber()[0];
+    }
+
+    /**
+     * @return int|mixed
+     */
+    private function getMaxNumberWhenNormalPoints()
+    {
         $groupDice = $this->dice->groupDice();
         //[{1,2},{3,2}]
         ksort($groupDice);
@@ -106,6 +161,5 @@ class Sibala
         }
         //預設值
         return 0;
-
     }
 }
