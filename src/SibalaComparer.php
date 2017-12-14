07bccfa651dcfe9823a2bbbdb2be8f38f763ab18
hatelove <jay.hatelove@gmail.com>
2017-12-14 15:33:08 +0800

    refactor: getComparer()

--- src/SibalaComparer.php
+++ src/SibalaComparer.php
@@ -26,17 +26,22 @@ class SibalaComparer
         if ($this->x->getState() !== $this->y->getState()) {
             return $this->x->state - $this->y->state;
         } else {
-
-            if ($this->x->getState() === Sibala::NO_POINTS) {
-                $comparer = new NoPointsComparer();
-                return $comparer->compare($this->x, $this->y);
-            } elseif ($this->x->getState() === Sibala::SAME_POINTS) {
-                $comparer = new SameColorComparer();
-                return $comparer->compare($this->x, $this->y);
-            }
-            $comparer = new NormalPointsComparer();
-            return $comparer->compare($this->x, $this->y);
+            return $this->getComparer()->compare($this->x, $this->y);
         }
+    }
+
+    /**
+     * @return NoPointsComparer|NormalPointsComparer|SameColorComparer
+     */
+    private function getComparer()
+    {
+        $comparerLookup = [
+            Sibala::NO_POINTS => new NoPointsComparer(),
+            Sibala::SAME_POINTS => new SameColorComparer(),
+            Sibala::N_POINTS => new NormalPointsComparer(),
+        ];
+
+        return $comparerLookup[$this->x->getState()];
     }
 
 
