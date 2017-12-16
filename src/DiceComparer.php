9d718d1c58d273fe37d6918a493ce4e482619033
hatelove <jay.hatelove@gmail.com>
2017-12-17 00:51:32 +0800

    refactor: extract method by each state

--- src/DiceComparer.php
+++ src/DiceComparer.php
@@ -27,27 +27,65 @@ class DiceComparer
     public function compare($x, $y)
     {
         if ($x->getType() != $y->getType()) {
-            return $x->getType() - $y->getType();
+            return $this->compareResultWhenStateIsDifferent($x, $y);
         }
 
         if ($x->getType() == Dice::NORMAL_POINTS) {
-            if ($x->getPoints() == $y->getPoints()) {
-                return $x->getMaxPoint() - $y->getMaxPoint();
-            }
-            return $x->getPoints() - $y->getPoints();
+            return $this->compareResultWhenNormalPoints($x, $y);
         }
         if ($x->getType() == Dice::SAME_COLOR) {
-            $sameColorWeightLookup = [
-                1 => 6,
-                4 => 5,
-                6 => 4,
-                5 => 3,
-                3 => 2,
-                2 => 1,
-            ];
-
-            return $sameColorWeightLookup[$x->getPoints()] - $sameColorWeightLookup[$y->getPoints()];
+            return $this->compareResultWhenSameColor($x, $y);
         }
+        return $this->compareResultWhenNoPoints();
+    }
+
+    /**
+     * @param $x
+     * @param $y
+     * @return mixed
+     */
+    private function compareResultWhenStateIsDifferent($x, $y)
+    {
+        return $x->getType() - $y->getType();
+    }
+
+    /**
+     * @param $x
+     * @param $y
+     * @return mixed
+     */
+    private function compareResultWhenNormalPoints($x, $y)
+    {
+        if ($x->getPoints() == $y->getPoints()) {
+            return $x->getMaxPoint() - $y->getMaxPoint();
+        }
+        return $x->getPoints() - $y->getPoints();
+    }
+
+    /**
+     * @param $x
+     * @param $y
+     * @return mixed
+     */
+    private function compareResultWhenSameColor($x, $y)
+    {
+        $sameColorWeightLookup = [
+            1 => 6,
+            4 => 5,
+            6 => 4,
+            5 => 3,
+            3 => 2,
+            2 => 1,
+        ];
+
+        return $sameColorWeightLookup[$x->getPoints()] - $sameColorWeightLookup[$y->getPoints()];
+    }
+
+    /**
+     * @return int
+     */
+    private function compareResultWhenNoPoints(): int
+    {
         return 0;
     }
 }
